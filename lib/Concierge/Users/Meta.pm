20484c113e42c0aead59efcc3962543bb1cb614b
Bruce Van Allen <bva@cruzio.com>
2026-07-16 12:27:29 -0700

    Release v0.9.3: remove required flag from created_date field
    The created_date system field is set internally by the service layer,
    not collected from user input, so callers consulting field hints via
    get_field_hints() should not be told it is required.
    
    Also fixes MANIFEST: exclude generated example-app runtime data
    (examples/*/data/) that had been committed to git and would otherwise
    ship inside the CPAN tarball. The example .pl scripts themselves are
    still shipped as before.

--- lib/Concierge/Users/Meta.pm
+++ lib/Concierge/Users/Meta.pm
@@ -1,4 +1,4 @@
-package Concierge::Users::Meta v0.9.2;
+package Concierge::Users::Meta v0.9.3;
 use v5.36;
 use Carp qw/ croak carp /;
 use YAML::Tiny;
@@ -1148,7 +1148,7 @@ utilities for Concierge::Users
 
 =head1 VERSION
 
-v0.9.2
+v0.9.3
 
 =head1 SYNOPSIS
 
@@ -1772,7 +1772,7 @@ __DATA__
 ################################################################################
 
 Configuration:
-  Version: v0.9.2
+  Version: v0.9.3
   Backend: Concierge::Users::Database  # Default; can be 'database', 'file', or 'yaml'
   Storage Directory: /path/to/storage  # Set during setup
   Generated: 2026-01-06 19:10:18
